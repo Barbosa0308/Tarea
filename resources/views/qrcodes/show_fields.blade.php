e63c80cc17938e6272b035cb47fdbfed0b8e1225
Barbosa0308 <fierrbar0308@gmail.com>
2023-09-20 19:01:25 -0500

    master

---
+++ resources/views/qrcodes/show_fields.blade.php
@@ -0,0 +1,60 @@
+<!-- User Id Field -->
+<div class="col-sm-12">
+    {!! Form::label('user_id', 'User Id:') !!}
+    <p>{{ $qrcode->user_id }}</p>
+</div>
+
+<!-- Website Field -->
+<div class="col-sm-12">
+    {!! Form::label('website', 'Website:') !!}
+    <p><a href>{{ $qrcode->website }}</a></p>
+</div>
+
+<!-- Company Name Field -->
+<div class="col-sm-12">
+    {!! Form::label('company_name', 'Company Name:') !!}
+    <p>{{ $qrcode->company_name }}</p>
+</div>
+
+<!-- Product Name Field -->
+<div class="col-sm-12">
+    {!! Form::label('product_name', 'Product Name:') !!}
+    <p>{{ $qrcode->product_name }}</p>
+</div>
+
+<!-- Product Url Field -->
+<div class="col-sm-12">
+    {!! Form::label('product_url', 'Product Url:') !!}
+    <p><a href>{{ $qrcode->product_url }}</a></p>
+</div>
+
+<!-- Product Url Image Path Field -->
+<div class="col-sm-12">
+    {!! Form::label('product_url_image_Path', 'Product Url Image Path:') !!}
+    <p>
+     <img src="{{asset($qrcode ->product_url_image_Path)}}" width="250px">
+    </p>
+</div>
+
+<!-- Callback Url Field -->
+<div class="col-sm-12">
+    {!! Form::label('callback_url', 'Callback Url:') !!}
+    <p><a href>{{ $qrcode->callback_url }}</a></p>
+</div>
+
+<!-- Qrcode Path Field -->
+<div class="col-sm-12">
+    {!! Form::label('qrcode_path', 'Qrcode Path:') !!}
+    <p>
+     <img src="{{asset($qrcode ->qrcode_path)}}" with="250PX" >
+    </p>
+    
+
+</div>
+
+<!-- Amount Field -->
+<div class="col-sm-12">
+    {!! Form::label('amount', 'Amount:') !!}
+    <p>{{ $qrcode->amount }}</p>
+</div>
+
